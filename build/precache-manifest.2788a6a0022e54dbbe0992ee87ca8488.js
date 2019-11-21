bf054e59c9f2cbadcc9f284db6e587df80efcff9
Petri Tuomola <petri@tuomola.org>
2019-11-21 22:36:03 +0400

    uibuild

---
+++ build/precache-manifest.2788a6a0022e54dbbe0992ee87ca8488.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13e7aaf530adc75aa036663d30d0230",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55242d7a06cf79b6f38e",
+    "url": "/static/js/2.011bd06c.chunk.js"
+  },
+  {
+    "revision": "3affabca4f4460f45858",
+    "url": "/static/js/main.6ec9be91.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
